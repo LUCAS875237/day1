ac4980bdbcb6b22dbaf1a9ae902521d5510fff1f
LUCAS875237 <lucas.renault@epitech.digital>
2024-09-30 17:02:38 +0200

    Create exercice4.js

---
+++ exercice4.js
@@ -0,0 +1,14 @@
+var undefine = null
+var créel = true
+var numéroté = 777
+var bigend = 1337n
+var symbolist = Symbol(symbolist)
+var unc = console.log
+
+displayThisText(typeof hvv)
+displayThisText(typeof créel)
+displayThisText(typeof numéroté)
+displayThisText(typeof bigend)
+displayThisText(typeof symbolist)
+displayThisText(typeof undefine);
+displayThisText(typeof unc)
