328f39e486e6f81120b4d34a5f2bba51a235bb7f
LUCAS875237 <lucas.renault@epitech.digital>
2024-09-30 17:03:59 +0200

    Create exercice6.js

---
+++ exercice6.js
@@ -0,0 +1,6 @@
+if (bananasCount < 0){
+  console.log(" Do I owe you bananas?"); }
+else if (bananasCount === 0) {
+  console.log("Oh no, there is no banana."); }
+else {
+  console.log("Yummy!"); }
