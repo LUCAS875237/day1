4108712f47787047a9773c02788f2ec6844896c2
LUCAS875237 <lucas.renault@epitech.digital>
2024-09-30 17:01:36 +0200

    Create exercice2.js

---
+++ exercice2.js
@@ -0,0 +1,11 @@
+var integer = 42
+var float = 42.42 
+var string = "forty two"
+var this_is_null = null
+var bool = true
+
+displayThisText(bool)
+displayThisText(string)
+displayThisText(integer)
+displayThisText(float)
+displayThisText(this_is_null)
